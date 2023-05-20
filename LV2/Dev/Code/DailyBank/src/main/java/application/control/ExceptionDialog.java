67b7ff08203af2a1266df4c72d67f426d5c430a3
evanl44730 <evan.l44730@gmail.com>
2023-05-20 17:57:08 +0200

    Ajout v2 code

---
+++ LV2/Dev/Code/DailyBank/src/main/java/application/control/ExceptionDialog.java
@@ -0,0 +1,65 @@
+package application.control;
+
+import application.DailyBankApp;
+import application.DailyBankState;
+import application.tools.StageManagement;
+import application.view.ExceptionDialogController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.orm.exception.ApplicationException;
+
+/**
+ * Classe de contrôleur pour la boîte de dialogue des exceptions.
+ */
+public class ExceptionDialog {
+
+	private Stage primaryStage;
+	private ExceptionDialogController edcViewController;
+
+	/**
+     * Constructeur de la classe ExceptionDialog.
+     * @param _parentStage le stage parent
+     * @param _dbstate l'état courant de l'application
+     * @param ae l'exception applicative
+     */
+	public ExceptionDialog(Stage _parentStage, DailyBankState _dbstate, ApplicationException ae) {
+
+		try {
+			FXMLLoader loader = new FXMLLoader(ExceptionDialogController.class.getResource("exceptiondialog.fxml"));
+			BorderPane root = loader.load();
+
+			Scene scene = new Scene(root, 700 + 20, 400 + 10);
+			scene.getStylesheets().add(DailyBankApp.class.getResource("application.css").toExternalForm());
+
+			this.primaryStage = new Stage();
+			this.primaryStage.initModality(Modality.WINDOW_MODAL);
+			this.primaryStage.initOwner(_parentStage);
+			StageManagement.manageCenteringStage(_parentStage, this.primaryStage);
+			this.primaryStage.setScene(scene);
+			this.primaryStage.setTitle("Opération impossible");
+			this.primaryStage.setResizable(false);
+
+			this.edcViewController = loader.getController();
+			this.edcViewController.initContext(this.primaryStage, _dbstate, ae);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+     * Affiche la boîte de dialogue des exceptions.
+     */
+	public void doExceptionDialog() {
+		this.edcViewController.displayDialog();
+	}
+
+	/*
+	 * Test : ApplicationException ae = new ApplicationException(Table.NONE,
+	 * Order.OTHER, "M", null ); ExceptionDialogTemp ed = new
+	 * ExceptionDialogTemp(primaryStage, dbs, ae); ed.doExceptionDisplay();
+	 */
+}
---
+++ LV2/Dev/Code/DailyBank/src/main/java/application/view/EmployesManagementController.java
@@ -0,0 +1,174 @@
+package application.view;
+
+import java.util.ArrayList;
+import model.orm.Access_BD_Employe;
+
+import application.DailyBankState;
+import application.control.EmployesManagement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import model.data.CompteCourant;
+import model.data.Employe;
+
+public class EmployesManagementController {
+
+	// Etat courant de l'application
+	private DailyBankState dailyBankState;
+
+	// Contrôleur de Dialogue associé à EmployesManagementController
+	private EmployesManagement cmDialogController;
+
+	// Fenêtre physique ou est la scène contenant le fichier xml contrôlé par this
+	private Stage primaryStage;
+
+	// Données de la fenêtre
+	private ObservableList<Employe> oListEmployes;
+
+	// Manipulation de la fenêtre
+	public void initContext(Stage _containingStage, EmployesManagement _cm, DailyBankState _dbstate) {
+		this.cmDialogController = _cm;
+		this.primaryStage = _containingStage;
+		this.dailyBankState = _dbstate;
+		this.configure();
+	}
+
+	private void configure() {
+		this.primaryStage.setOnCloseRequest(e -> this.closeWindow(e));
+
+		this.oListEmployes = FXCollections.observableArrayList();
+		this.lvEmployes.setItems(this.oListEmployes);
+		this.lvEmployes.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		this.lvEmployes.getFocusModel().focus(-1);
+		this.lvEmployes.getSelectionModel().selectedItemProperty().addListener(e -> this.validateComponentState());
+		this.validateComponentState();
+	}
+
+	public void displayDialog() {
+		this.primaryStage.showAndWait();
+	}
+
+	// Gestion du stage
+	private Object closeWindow(WindowEvent e) {
+		this.doCancel();
+		e.consume();
+		return null;
+	}
+
+	// Attributs de la scene + actions
+
+	@FXML
+	private TextField txtNum;
+	@FXML
+	private TextField txtNom;
+	@FXML
+	private TextField txtPrenom;
+	@FXML
+	private ListView<Employe> lvEmployes;
+	@FXML
+	private Button btnDesactEmploye;
+	@FXML
+	private Button btnModifEmploye;
+
+	@FXML
+	private void doCancel() {
+		this.primaryStage.close();
+	}
+
+	@FXML
+	private void doRechercher() {
+		int numCompte;
+		try {
+			String nc = this.txtNum.getText();
+			if (nc.equals("")) {
+				numCompte = -1;
+			} else {
+				numCompte = Integer.parseInt(nc);
+				if (numCompte < 0) {
+					this.txtNum.setText("");
+					numCompte = -1;
+				}
+			}
+		} catch (NumberFormatException nfe) {
+			this.txtNum.setText("");
+			numCompte = -1;
+		}
+
+		String debutNom = this.txtNom.getText();
+		String debutPrenom = this.txtPrenom.getText();
+
+		if (numCompte != -1) {
+			this.txtNom.setText("");
+			this.txtPrenom.setText("");
+		} else {
+			if (debutNom.equals("") && !debutPrenom.equals("")) {
+				this.txtPrenom.setText("");
+			}
+		}
+
+		// Recherche des employés en BD. cf. AccessEmploye > getEmployes(.)
+		// numCompte != -1 => recherche sur numCompte
+		// numCompte != -1 et debutNom non vide => recherche nom/prenom
+		// numCompte != -1 et debutNom vide => recherche tous les employés
+		ArrayList<Employe> listeCli;
+		listeCli = this.cmDialogController.getlisteComptes(numCompte, debutNom, debutPrenom);
+
+		this.oListEmployes.clear();
+		this.oListEmployes.addAll(listeCli);
+		this.validateComponentState();
+	}
+
+	@FXML
+	private void doModifierEmploye() {
+
+		int selectedIndice = this.lvEmployes.getSelectionModel().getSelectedIndex();
+		if (selectedIndice >= 0) {
+			Employe cliMod = this.oListEmployes.get(selectedIndice);
+			Employe result = this.cmDialogController.modifierEmploye(cliMod);
+			if (result != null) {
+				this.oListEmployes.set(selectedIndice, result);
+			}
+		}
+	}
+
+	@FXML
+	private void doSupprimerEmploye() {
+		int selectedIndice = this.lvEmployes.getSelectionModel().getSelectedIndex();
+		if (selectedIndice >= 0) {
+			Employe cliMod = this.oListEmployes.get(selectedIndice);
+			this.cmDialogController.supprimerEmploye(cliMod);
+			this.oListEmployes.remove(selectedIndice);
+		}
+		this.validateComponentState();
+	}
+
+	@FXML
+	private void doNouveauEmploye() {
+		Employe employe;
+		employe = this.cmDialogController.nouveauEmploye();
+		if (employe != null) {
+			this.oListEmployes.add(employe);
+		}
+	}
+
+	private void validateComponentState() {
+		// Non implémenté => désactivé
+		this.btnDesactEmploye.setDisable(true);
+		int selectedIndice = this.lvEmployes.getSelectionModel().getSelectedIndex();
+		if (selectedIndice >= 0) {
+			this.btnModifEmploye.setDisable(false);
+			this.btnDesactEmploye.setDisable(false);
+		} else {
+			this.btnModifEmploye.setDisable(true);
+			this.btnDesactEmploye.setDisable(true);
+		}
+	}
+}
