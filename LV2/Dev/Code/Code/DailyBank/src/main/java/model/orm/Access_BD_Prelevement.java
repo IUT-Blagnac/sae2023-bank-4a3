aa8e80baa12abd2aed1f1820406b7121a5998963
evanl44730 <evan.l44730@gmail.com>
2023-05-25 16:51:30 +0200

    Ajout prelevement

---
+++ LV2/Dev/Code/Code/DailyBank/src/main/java/model/orm/Access_BD_Prelevement.java
@@ -0,0 +1,65 @@
+package model.orm;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.data.Client;
+import model.data.Prelevement;
+import model.orm.exception.DataAccessException;
+import model.orm.exception.DatabaseConnexionException;
+import model.orm.exception.Order;
+import model.orm.exception.RowNotFoundOrTooManyRowsException;
+import model.orm.exception.Table;
+
+public class Access_BD_Prelevement {
+	
+	public Access_BD_Prelevement(){
+		
+	}
+
+	public void insertPrelevement(Prelevement pre)
+			throws RowNotFoundOrTooManyRowsException, DataAccessException, DatabaseConnexionException {
+		try {
+
+			Connection con = LogToDatabase.getConnexion();
+
+			String query = "INSERT INTO PRELEVEMENTAUTOMATIQUE VALUES (" + "seq_id_prelevauto.NEXTVAL" + ", " + "?" + ", " + "?" + ", "
+					+ "?" + ", " + "?" + ")";
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setInt(1, pre.montant);
+			pst.setInt(2, pre.date);
+			pst.setString(3, pre.beneficiaire);
+			pst.setInt(4, pre.idNumCompte);
+
+			System.err.println(query);
+
+			int result = pst.executeUpdate();
+			pst.close();
+
+			if (result != 1) {
+				con.rollback();
+				throw new RowNotFoundOrTooManyRowsException(Table.PrelevementAutomatique, Order.INSERT,
+						"Insert anormal (insert de moins ou plus d'une ligne)", null, result);
+			}
+
+			query = "SELECT seq_id_prelevauto.CURRVAL from DUAL";
+
+			System.err.println(query);
+			PreparedStatement pst2 = con.prepareStatement(query);
+
+			ResultSet rs = pst2.executeQuery();
+			rs.next();
+			//int numCliBase = rs.getInt(1);
+
+			con.commit();
+			rs.close();
+			pst2.close();
+
+			//client.idNumCli = numCliBase;
+		} catch (SQLException e) {
+			throw new DataAccessException(Table.PrelevementAutomatique, Order.INSERT, "Erreur accès", e);
+		}
+	}
+}
